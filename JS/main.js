61882aae84e20c0d47042d84ebe43de769336842
maurizio <maurizio.grasso@gmail.com>
2021-02-25 19:30:50 +0100

    Matrix

--- JS/main.js
+++ JS/main.js
@@ -1,22 +1,31 @@
-for (i = 0; i< 100; i++) {   
-    if ( ( (i + 1) % 3 == 0 ) && ( (i + 1) % 5 == 0 ) ) {
-        // Se il valore di 'i + 1' è sia multiplo di 3 che multiplo di 5
-        console.log("FizzBuzz");
-        document.getElementById("populate-list").innerHTML += "<li>FizzBuzz</li>";
+// recupera le variabili dall'input dell'utente
+
+var fizzValue  = document.getElementById("input-fizz").value;
+var buzzValue  = document.getElementById("input-buzz").value;
+var iterazioni = document.getElementById("input-iterations").value;
+
+for (i = 0; i< iterazioni; i++) {   
+    if ( ( (i + 1) % fizzValue == 0 ) && ( (i + 1) % buzzValue == 0 ) ) {
+        // Se il valore di 'i + 1' è sia multiplo di 'fuzz' che multiplo di 'buzz'
+        document.getElementById("populate-list").innerHTML += "<li class=\"text-secondary\">FizzBuzz</li>";
     }
-    else if ((i+1) % 3 == 0) {
-        // Se il valore di 'i + 1' è multiplo di 3
-        console.log("Fizz");
+    else if ((i+1) % fizzValue == 0) {
+        // Se il valore di 'i + 1' è multiplo di 'fizz'
         document.getElementById("populate-list").innerHTML += "<li>Fizz</li>";
     }
-    else if ((i+1) % 5 == 0) {
-        // Se il valore di 'i + 1' è multiplo di 5
-        console.log("Buzz");
+    else if ((i+1) % buzzValue == 0) {
+        // Se il valore di 'i + 1' è multiplo di 'buzz'
         document.getElementById("populate-list").innerHTML += "<li>Buzz</li>";
     }
     else {
-        // Qualsiasi altra condizione (ergo: se il valore di 'i + 1' non è né multplo di 3 né multiplo di 5)
-        console.log(i+1);
+        // Qualsiasi altra condizione (ergo: se il valore di 'i + 1' non è né multplo di 'fizz' né multiplo di 'buzz')
         document.getElementById("populate-list").innerHTML += "<li>"+(i+1)+"</li>";
     }
 }
+
+// Pulsante 'Aggiorna'
+document.getElementById("refresh-button").addEventListener("click" , function() {
+    location.reload();
+}
+)
+
