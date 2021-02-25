a1da97187b2e10dac11310053d7d11b6ffb857f6
maurizio <maurizio.grasso@gmail.com>
2021-02-25 16:37:42 +0100

    grafica

--- JS/main.js
+++ JS/main.js
@@ -2,17 +2,21 @@ for (i = 0; i< 100; i++) {
     if ( ( (i + 1) % 3 == 0 ) && ( (i + 1) % 5 == 0 ) ) {
         // Se il valore di 'i + 1' è sia multiplo di 3 che multiplo di 5
         console.log("FizzBuzz");
+        document.getElementById("populate-list").innerHTML += "<li>FizzBuzz</li>";
     }
     else if ((i+1) % 3 == 0) {
         // Se il valore di 'i + 1' è multiplo di 3
         console.log("Fizz");
+        document.getElementById("populate-list").innerHTML += "<li>Fizz</li>";
     }
     else if ((i+1) % 5 == 0) {
         // Se il valore di 'i + 1' è multiplo di 5
         console.log("Buzz");
+        document.getElementById("populate-list").innerHTML += "<li>Buzz</li>";
     }
     else {
         // Qualsiasi altra condizione (ergo: se il valore di 'i + 1' non è né multplo di 3 né multiplo di 5)
         console.log(i+1);
+        document.getElementById("populate-list").innerHTML += "<li>"+(i+1)+"</li>";
     }
 }
