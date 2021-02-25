278d5f3cd49e44d570ec85da0bd6c0102c7c18ff
maurizio <maurizio.grasso@gmail.com>
2021-02-25 20:29:33 +0100

    BugsFix
    Risolti alcuni bug

--- JS/main.js
+++ JS/main.js
@@ -1,31 +1,32 @@
-// recupera le variabili dall'input dell'utente
+var fizzValue , buzzValue , iterazioni; // variabili che saranno inserite dall'utente
+var idLista = document.getElementById("populate-list"); // variabile che fa riferimento alla <ul> contenente la lista
 
-var fizzValue  = document.getElementById("input-fizz").value;
-var buzzValue  = document.getElementById("input-buzz").value;
-var iterazioni = document.getElementById("input-iterations").value;
-
-for (i = 0; i< iterazioni; i++) {   
-    if ( ( (i + 1) % fizzValue == 0 ) && ( (i + 1) % buzzValue == 0 ) ) {
-        // Se il valore di 'i + 1' è sia multiplo di 'fuzz' che multiplo di 'buzz'
-        document.getElementById("populate-list").innerHTML += "<li class=\"text-secondary\">FizzBuzz</li>";
-    }
-    else if ((i+1) % fizzValue == 0) {
-        // Se il valore di 'i + 1' è multiplo di 'fizz'
-        document.getElementById("populate-list").innerHTML += "<li>Fizz</li>";
-    }
-    else if ((i+1) % buzzValue == 0) {
-        // Se il valore di 'i + 1' è multiplo di 'buzz'
-        document.getElementById("populate-list").innerHTML += "<li>Buzz</li>";
-    }
-    else {
-        // Qualsiasi altra condizione (ergo: se il valore di 'i + 1' non è né multplo di 'fizz' né multiplo di 'buzz')
-        document.getElementById("populate-list").innerHTML += "<li>"+(i+1)+"</li>";
-    }
-}
-
-// Pulsante 'Aggiorna'
+// Cliccando sul Pulsante...
 document.getElementById("refresh-button").addEventListener("click" , function() {
-    location.reload();
-}
-)
+    
+    idLista.innerHTML = ""; // Resetta la lista (potrebbe contenere elementi generati da eventuali cicli precedenti)
 
+    fizzValue  = document.getElementById("input-fizz").value;         // Recupera valore fizz
+    buzzValue  = document.getElementById("input-buzz").value;         // Recupera valore buzz
+    iterazioni = document.getElementById("input-iterations").value;   // Recupera valore iterazioni
+
+    for (i = 0; i< iterazioni; i++) {
+        if ( ( (i + 1) % fizzValue == 0 ) && ( (i + 1) % buzzValue == 0 ) ) {
+            // Se il valore di 'i + 1' è sia multiplo di 'fuzz' che multiplo di 'buzz'
+            idLista.innerHTML += "<li class=\"text-secondary\">FizzBuzz</li>";
+        }
+        else if ((i+1) % fizzValue == 0) {
+            // Se il valore di 'i + 1' è multiplo di 'fizz'
+            idLista.innerHTML += "<li>Fizz</li>";
+        }
+        else if ((i+1) % buzzValue == 0) {
+            // Se il valore di 'i + 1' è multiplo di 'buzz'
+            idLista.innerHTML += "<li>Buzz</li>";
+        }
+        else {
+            // Qualsiasi altra condizione (ergo: se il valore di 'i + 1' non è né multplo di 'fizz' né multiplo di 'buzz')
+            idLista.innerHTML += "<li>"+(i+1)+"</li>";
+        }
+    }   //  Chiusura ciclo for
+}   //  Chiusura Funzione Anonima
+)   //  Chiusura parametri Event Listener
